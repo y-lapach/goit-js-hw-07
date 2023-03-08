4508c3200a01eb21f4d51006a8edc928f5fe92c1
Yana Lapach <y.lapach@gmail.com>
2023-03-09 00:11:43 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const gallerySet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" style="display:block;"/>
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', gallerySet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
